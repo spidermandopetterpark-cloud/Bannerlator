d5cf9687700b0fea1df27b6f1611b27072f15864
spidermandopetterpark-cloud <spidermandopetterpark@gmail.com>
2026-08-17 21:29:28 -0300

    Update SystemSettings.java

--- app/src/main/java/com/winlator/star/SystemSettings.java
+++ app/src/main/java/com/winlator/star/SystemSettings.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.titanpc.R;
+import com.winlator.star.R;
 
 public class SystemSettings extends AppCompatActivity {
 
